72b5d63f079c2a57729ed93b90bab85d3b7b087a
Dhrupad Rajpurohit <dhrupadrajpurohit@gmail.com>
2025-07-14 20:38:07 +0000

    remove stududent-login completely

---
+++ src/app/student-login/layout.tsx
@@ -0,0 +1,8 @@
+// This layout is intentionally left blank.
+export default function StudentLoginLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
---
+++ src/app/student-signup/layout.tsx
@@ -0,0 +1,8 @@
+// This layout is intentionally left blank.
+export default function StudentSignupLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
