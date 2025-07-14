9cb82aae28adae8e60af55defb7071f2b4be9284
Dhrupad Rajpurohit <dhrupadrajpurohit@gmail.com>
2025-07-14 20:39:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/signup/layout.tsx
+++ src/app/signup/layout.tsx
@@ -1,12 +1,14 @@
 
+import { AuthProvider } from "@/contexts/auth-provider";
+
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <AuthProvider>
         {children}
-    </>
+    </AuthProvider>
   );
 }
